90e9e5e466c637af733821bdb6f72fc7392a696b
Azmi <35288367+NafisatulAzmi@users.noreply.github.com>
2018-01-30 17:54:29 +0700

    Create root-pass.sh

---
+++ root-pass.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#Script Created By Pa'an Finest
+echo -e "\e[032;1m---------- Mengganti Root\e[0m \e[034;1mPassword ----------\e[0m"
+# begin of user-list
+#echo "-----------------------------------"
+#echo "USERNAME              EXP DATE     "
+#echo "-----------------------------------"
+
+#while read expired
+#do
+#	AKUN="$(echo $expired | cut -d: -f1)"
+#	ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
+#	exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+#	if [[ $ID -ge 1000 ]]; then
+#		printf "%-21s %2s\n" "$AKUN" "$exp"
+#	fi
+#done < /etc/passwd
+#echo "-----------------------------------"
+#echo ""
+# end of user-list
+
+#read -p "Isikan username: " username
+
+username="root"
+#egrep "^$username" /etc/passwd >/dev/null
+#if [ $? -eq 0 ]; then
+	read -p "Isikan password baru akun [$username]: " password
+	read -p "Konfirmasi password baru akun [$username]: " password1
+	echo ""
+	if [[ $password = $password1 ]]; then
+		echo $username:$password | chpasswd
+		echo "Penggantian password akun [$username] Sukses"
+		echo ""
+	    echo "------------------------------------------"
+	    echo "Informasi Detail Akun Anda :"
+	    echo "------------------------------------------"
+	    echo "Host/IP: $MYIP"
+	    echo "Username : $username"
+	    echo "Password : $password"
+	    echo "OpenSSH Port : 22, 143"
+      echo "SSL/TSL Port : 443"
+    	echo "Squid Proxy : 80, 8080, 3128"
+	    echo "OpenVPN Config : http://$MYIP:81/client.ovpn"
+	    echo "Aktif Sampai : $(date -d "$AKTIF days" +"%d-%m-%Y")"
+       echo "------------------------------------------"
+       echo -e "\e[032;1mScript Created\e[0m \e[034;1mBy Pa'an Finest\e[032;1m"
+       echo ""
+       echo -e" \e[032;1m[ Facebook : Pa'an\e[0m | \e[031;1mInstagram : @paan_finest\e[0m | \e[034;1mWA : +1 (202) 852-2868 ]\e[0m"
+fi
+
+	else
+		echo "Penggantian password akun [$username] Gagal"
+		echo "[Password baru] & [Konfirmasi Password Baru] tidak cocok, silahkan ulangi lagi!"
+	fi
+#else
+#	echo "Username [$username] belum terdaftar!"
+#	exit 1
+#fi
