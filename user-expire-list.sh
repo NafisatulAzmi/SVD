8a2320a715ba68f7b34d4085eda3b0a635072bb8
Azmi <35288367+NafisatulAzmi@users.noreply.github.com>
2018-01-30 18:15:56 +0700

    Create user-expire-list.sh

---
+++ user-expire-list.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Script Created By Pa'an Finest
+disable-user-expire
+clear
+
+echo -e "\e[032;1m---------- Daftar Akun\e[0m \e[034;1mExpire----------\e[0m"
+cat /root/expireduser.txt
+echo ""
