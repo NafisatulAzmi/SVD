4e7a456f4a51acc23d4505c24ffde3b56a7bdde5
Azmi <35288367+NafisatulAzmi@users.noreply.github.com>
2018-01-30 17:18:10 +0700

    Create banned-user.sh

---
+++ banned-user.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#Script Created By Pa'an Finest
+# begin of user-list
+echo -e "\e[032;1m---------- Banned\e[0m \e[034;1m User----------\e[0m"
+echo "-----------------------------------"
+echo "USERNAME              EXP DATE     "
+echo "-----------------------------------"
+
+while read expired
+do
+	AKUN="$(echo $expired | cut -d: -f1)"
+	ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
+	exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+	if [[ $ID -ge 1000 ]]; then
+		printf "%-21s %2s\n" "$AKUN" "$exp"
+	fi
+done < /etc/passwd
+echo "-----------------------------------"
+echo ""
+# end of user-list
+
+read -p "Isikan username: " username
+
+egrep "^$username" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+	echo ""
+	read -p "Apakah Anda benar-benar ingin mem-banned akaun [$username] [y/n]: " -e -i y BANNED
+	if [[ "$BANNED" = 'y' ]]; then
+		echo " User : $username" >> /root/banneduser.txt
+		passwd -l $username
+		echo ""
+		echo "Akaun [$username] berhasil dibanned!"
+	else
+		echo ""
+		echo "Banned akun [$username] dibatalkan!"
+	fi
+else
+	echo "Username [$username] belum terdaftar!"
+	exit 1
+fi 
