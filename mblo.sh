4d0f679474fc7aa94ac26a21243621332bb54ea4
Azmi <35288367+NafisatulAzmi@users.noreply.github.com>
2018-02-01 12:05:26 +0700

    Update and rename san.sh to mblo.sh

--- mblo.sh
+++ mblo.sh
@@ -10,7 +10,7 @@ else
 	read -p "Isikan password akun [$username]: " password
 	read -p "Berapa hari akun [$username] aktif: " AKTIF
 
-        wget icanhazip.com -qO -
+        $IP: wget icanhazip.com -qO -
 	today="$(date +"%Y-%m-%d")"
 	expire=$(date -d "$AKTIF days" +"%Y-%m-%d")
 	useradd -M -N -s /bin/false -e $expire $username
