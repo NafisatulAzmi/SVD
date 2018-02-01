9a498f118133518c64a07cf94f02531063dfc7da
Azmi <35288367+NafisatulAzmi@users.noreply.github.com>
2018-02-01 12:42:15 +0700

    Update and rename ini.sh to i.sh

--- i.sh
+++ i.sh
@@ -3,7 +3,7 @@ while :
 do
 	#MYIP=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
 	#if [ "$MYIP" = "" ]; then
-		#MYIP=$(wget -qO- ipv4.icanhazip.com)
+		#MYIP=$(wget icanhazip.com -qO -)
 	#fi
 
 	clear
