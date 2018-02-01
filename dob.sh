a2913459a28d8eb0b8ae57c5cf07af2edb6ff3c4
Azmi <35288367+NafisatulAzmi@users.noreply.github.com>
2018-02-01 12:00:48 +0700

    Update and rename buatssh.sh to dob.sh

--- dob.sh
+++ dob.sh
@@ -10,7 +10,7 @@ else
 	read -p "Isikan password akun [$username]: " password
 	read -p "Berapa hari akun [$username] aktif: " AKTIF
 
-IP=`dig +short myip.opendns.com @resolver1.opendns.com`
+        IP="$IP(date wget icanhazip.com -qO -")"
 	today="$(date +"%Y-%m-%d")"
 	expire=$(date -d "$AKTIF days" +"%Y-%m-%d")
 	useradd -M -N -s /bin/false -e $expire $username
